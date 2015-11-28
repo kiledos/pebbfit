5701b253f4dd40d4f6cb1512f3995e3516cbd05a
kiledos <richardzhang96@gmail.com>
2015-11-28 02:43:55 -0500

    Timer

--- src/main.c
+++ src/main.c
@@ -4,27 +4,41 @@ static Window *s_main_window;
 static TextLayer *s_time_layer;
 TextLayer *text_layer;
 
+static int s_uptime = 0;
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  // Use a long-lived buffer
+  static char s_uptime_buffer[32];
+
+  // Get time since launch
+  int seconds = s_uptime % 60;
+  int minutes = (s_uptime % 3600) / 60;
+  int hours = s_uptime / 3600;
+
+  // Update the TextLayer
+  snprintf(s_uptime_buffer, sizeof(s_uptime_buffer), "Timer: %dh %dm %ds", hours, minutes, seconds);
+  text_layer_set_text(s_time_layer, s_uptime_buffer);
+
+  // Increment s_uptime
+  s_uptime++;
+}
+
 static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
   // Create the TextLayer with specific bounds
-  s_time_layer = text_layer_create(
-  GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
-
-  // Improve the layout to be more like a watchface
-  text_layer_set_background_color(s_time_layer, GColorBlack);
-  text_layer_set_text_color(s_time_layer, GColorClear);
-  text_layer_set_text(s_time_layer, "00:00");
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  s_time_layer = text_layer_create(GRect(0,0, bounds.size.w, bounds.size.h));
+   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  text_layer_set_text(s_time_layer, "Timer: 0h 0m 0s");
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 }
 
 static void main_window_unload(Window *window) {
-
+text_layer_destroy(s_time_layer);
 }
 
 void init(void) {
@@ -35,6 +49,8 @@ void init(void) {
   });
   text_layer = text_layer_create(GRect(0, 0, 144, 20));
   window_stack_push(s_main_window, true);
+  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
+  
 }
 
 void deinit(void) {
