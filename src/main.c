83ed84e961489ecb143f91f047aab6f54e0f17c5
kiledos <richardzhang96@gmail.com>
2015-11-28 03:51:33 -0500

    Accelerometer Added

--- src/main.c
+++ src/main.c
@@ -7,8 +7,10 @@ TextLayer *text_layer;
 static int s_uptime = 0; //Timer using tick_handler
 static int start = 0; //Start/stop the timer
 
+
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
  text_layer_set_text(s_time_layer, "Workout start");
+ 
  start = 1;
 }
 
@@ -31,7 +33,7 @@ static void click_config_provider(void *context) {
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   // Use a long-lived buffer
   static char s_uptime_buffer[32];
-
+  
   // Get time since launch
   int seconds = s_uptime % 60;
   int minutes = (s_uptime % 3600) / 60;
@@ -65,6 +67,22 @@ static void main_window_unload(Window *window) {
 text_layer_destroy(s_time_layer);
 }
 
+static void data_handler(AccelData *data, uint32_t num_samples) {
+  // Long lived buffer
+  static char s_buffer[128];
+
+  // Compose string of all data for 3 samples
+  snprintf(s_buffer, sizeof(s_buffer), 
+    "N X,Y,Z\n0 %d,%d,%d\n1 %d,%d,%d\n2 %d,%d,%d", 
+    data[0].x, data[0].y, data[0].z, 
+    data[1].x, data[1].y, data[1].z, 
+    data[2].x, data[2].y, data[2].z
+  );
+
+  //Show the data
+  printf("%s",s_buffer);
+}
+
 void init(void) {
   s_main_window = window_create();  
   window_set_window_handlers(s_main_window, (WindowHandlers) {
@@ -73,12 +91,17 @@ void init(void) {
   });
   text_layer = text_layer_create(GRect(0, 0, 144, 20));
   window_stack_push(s_main_window, true);
-  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
-  
-   //Register click handlers
+  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);  
+ 
+  //Register click handlers
   window_set_click_config_provider(s_main_window, click_config_provider);
-
+  
+  //Start accel service
+  uint32_t num_samples = 3;  
+  accel_data_service_subscribe(num_samples, data_handler);
+  accel_service_set_sampling_rate(ACCEL_SAMPLING_10HZ);
 }
+
 
 void deinit(void) {
   text_layer_destroy(text_layer);
